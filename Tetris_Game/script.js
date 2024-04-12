723f458c5b618672aea7e39f9218716698fc052b
Huy Nguyen <161923496+jussahug@users.noreply.github.com>
2024-04-12 15:07:54 +0700

    Add files via upload
    Create

---
+++ Tetris_Game/script.js
@@ -0,0 +1,93 @@
+const tetrominos = {
+    'I': [
+        [0,0,0,0],
+        [1,1,1,1],
+        [0,0,0,0],
+        [0,0,0,0]
+    ],
+    'J': [
+        [1,0,0],
+        [1,1,1],
+        [0,0,0],
+    ],
+    'L': [
+        [0,0,1],
+        [1,1,1],
+        [0,0,0],
+    ],
+    'O': [
+        [1,1],
+        [1,1],
+    ],
+    'S': [
+        [0,1,1],
+        [1,1,0],
+        [0,0,0],
+    ],
+    'Z': [
+        [1,1,0],
+        [0,1,1],
+        [0,0,0],
+    ],
+    'T': [
+        [0,1,0],
+        [1,1,1],
+        [0,0,0],
+    ]
+};
+
+// mau gach
+const colors = {
+    'I': 'cyan',
+    'O': 'yellow',
+    'T': 'purple',
+    'S': 'green',
+    'Z': 'red',
+    'J': 'blue',
+    'L': 'orange'
+};
+
+let count = 0;
+let tetromino = getNextTetromino();
+let rAF = null;  // keep track of the animation frame so we can cancel it
+let gameOver = false;
+
+
+
+const canvas = document.getElementById('game');
+const context = canvas.getContext('2d');
+const grid = 32;
+const tetrominoSequence = [];
+
+
+function getRandomInt(min, max) {
+
+}
+
+function generateSequence() {
+
+}
+
+function getNextTetromino() {
+
+}
+
+function rotate(matrix) {
+
+}
+
+function isValidMove(matrix, cellRow, cellCol) {
+
+}
+
+function placeTetromino() {
+
+}
+
+function showGameOver() {
+
+}
+
+
+
+
